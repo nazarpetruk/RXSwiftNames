7280df443a4d8b3ef3613b2d639cb6643b19507a
nazar petruk <nazarpetruk15@gmail.com>
2020-01-23 15:11:21 +0100

    Initial Commit

---
+++ RXSwiftNames/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RXSwiftNames
+//
+//  Created by Nazar Petruk on 23/01/2020.
+//  Copyright © 2020 Nazar Petruk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
