033e30af4442b580bdb9d10cc883204b09bd3386
nazar petruk <nazarpetruk15@gmail.com>
2020-01-23 16:06:33 +0100

    - starting reactive programming - adding func for binding text field with top latel - adding bounce delay for 0.5 sec

--- RXSwiftNames/ViewController.swift
+++ RXSwiftNames/ViewController.swift
@@ -7,14 +7,36 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
+    
+    //MARK: IBOutlets
+    @IBOutlet weak var helloLbl: UILabel!
+    @IBOutlet weak var nameEntryTextField: UITextField!
+    @IBOutlet weak var submitButton: UIButton!
+    @IBOutlet weak var namesLbl: UILabel!
+    
+    
+    let disposeBag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        bind()
     }
-
-
+    
+    func bind() {
+        nameEntryTextField.rx.text.debounce(0.5, scheduler: MainScheduler.instance)
+            .map{
+                if $0 == ""{
+                    return "Type your name below."
+                }else{
+                    return "HELLO, \($0!)."
+                }
+        }
+        .bind(to: helloLbl.rx.text)
+        .disposed(by: disposeBag)
+    }
 }
 
